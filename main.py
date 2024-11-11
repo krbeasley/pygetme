26f17be13ab4ad970f7d6680e12ce434a3839efb
Kyle Beasley <kyle.beasley17@gmail.com>
2024-11-11 03:33:47 -0600

    added uptime support and fixed the size of a gigabyte

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 import subprocess
 import socket
 
-GIGABYTE = 1000000000
+GIGABYTE = 1024 * 1024 * 1024
 
 def clear() : 
     subprocess.run("clear")
@@ -26,6 +26,33 @@ def getIP() :
     
     return IP
 
+intervals = (
+    ('weeks', 604800),  # 60 * 60 * 24 * 7
+    ('days', 86400),    # 60 * 60 * 24
+    ('hours', 3600),    # 60 * 60
+    ('minutes', 60),
+    ('seconds', 1),
+)
+
+def displayTime(seconds, granularity=2):
+    result = []
+
+    for name, count in intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            if value == 1:
+                name = name.rstrip('s')
+            result.append("{} {}".format(value, name))
+    return ', '.join(result[:granularity])
+
+def getBootTime() :
+    s = subprocess.getoutput('sysctl -a | grep "kern.boottime"').split(": ")[1]
+
+    boot = s.split(',')[0].split(" = ")[1]
+
+    return int(boot)
+
 if __name__ == "__main__" :
     clear()
 
@@ -40,14 +67,18 @@ if __name__ == "__main__" :
     CPU_CORES = subprocess.getoutput('sysctl -a | grep "cpu.core_count"').split(": ")[1]
 
     # get ram
-    RAM_AMT = str(int(subprocess.getoutput('sysctl -a | grep "hw.memsize_usable:"').split(": ")[1]) / GIGABYTE)[0:4]
+    RAM_AMT = str(int(subprocess.getoutput('sysctl -a | grep "hw.memsize:"').split(": ")[1]) / GIGABYTE)[0:4]
 
     # get local ip address
     IP = getIP()
 
     # get uptime
+    NOW = int(subprocess.getoutput('date +%s'))
+    BOOT = getBootTime()
+    UPTIME = displayTime(NOW - BOOT, 4)
 
 
     # echo it all out
     print(f"USER: {USER}    MACHINE: {MACHINE}    LEAKED IP: {IP}")
     print(f"CPU: {CPU_NAME}    CORES: {CPU_CORES}    RAM: {RAM_AMT} GB")
+    print(f"UPTIME: {UPTIME}")
